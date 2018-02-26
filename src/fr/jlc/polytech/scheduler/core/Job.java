01a2acab04394057a0cb9ac14ba681b75c5057a9
Philippine CLUNIAT <philippine.cluniat@gmail.com>
2018-02-26 10:04:27 +0100

    Job debut de create

--- src/fr/jlc/polytech/scheduler/core/Job.java
+++ src/fr/jlc/polytech/scheduler/core/Job.java
@@ -4,8 +4,10 @@ import com.sun.istack.internal.Nullable;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Job extends ArrayList<Task> implements Serializable {
+
 	
 	public Job(@Nullable Task... tasks) {
 		super(tasks != null ? tasks.length : 3);
@@ -18,4 +20,16 @@ public class Job extends ArrayList<Task> implements Serializable {
 	public Job(int initialCapacity) {
 		super(initialCapacity);
 	}
+
+	public ArrayList<Task> create(){
+        Random r = new Random();
+        ArrayList<Task> list_task = new ArrayList<Task>();
+        int nb_task = r.nextInt(10000);
+        for (int i = 0; i <nb_task ; i++) {
+            Type type = Type.values()[r.nextInt(Type.values().length)];
+            int capacity =
+            predecessor = list_task.get(r.nextInt(list_task.size()));
+            Task task = new Task(, )
+        }
+    }
 }
