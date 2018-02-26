f028ce588253553f2163b247abdb3289ea5e8cd0
Valentin Berger <Valentin.berger38@gmail.com>
2018-02-26 10:25:03 +0100

    Type String & Tests
    * Added 'toString()' in 'Type' (enum)
    * Added 'test' folder (for tests)
    * Added 'lib' folder (for libraries)
    * Added JUnit5.0 library (in 'lib' folder)
    * Added FileGeneratorTest class

---
+++ test/fr/jlc/polytech/scheduler/io/FileGeneratorTest.java
@@ -0,0 +1,19 @@
+package fr.jlc.polytech.scheduler.io;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileGeneratorTest {
+	
+	@BeforeEach
+	void setup() {
+		// TODO: Create same test as slide
+	}
+	
+	@Test
+	void test_generateContent() {
+		// TODO: Test
+	}
+}
