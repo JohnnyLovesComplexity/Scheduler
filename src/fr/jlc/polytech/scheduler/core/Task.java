9f80d9f235b83f01f326b440317b80ba6e8ce205
Philippine CLUNIAT <philippine.cluniat@gmail.com>
2018-02-26 09:10:28 +0100

    Task update

--- src/fr/jlc/polytech/scheduler/core/Task.java
+++ src/fr/jlc/polytech/scheduler/core/Task.java
@@ -1,6 +1,29 @@
 package fr.jlc.polytech.scheduler.core;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Task implements Serializable {
+    private String type;
+    private int capacity;
+    private ArrayList<Task> dependencies;
+
+    public Task(String type, int capacity, ArrayList<Task> dependencies) {
+        this.type = type;
+        this.capacity = capacity;
+        this.dependencies = dependencies;
+    }
+
+    public boolean detectInfiniteLoops(Task t, ArrayList<Task> visited){
+        if (visited.contains(t)){
+            return true;
+        }
+        visited.add(t);
+        for (Task p : t.dependencies) {
+            if (detectInfiniteLoops(p, visited)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
