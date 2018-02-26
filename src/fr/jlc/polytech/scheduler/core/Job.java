88a66af3ef260d5986b6ea5f52f9b5ebcb113fe1
Valentin Berger <Valentin.berger38@gmail.com>
2018-02-26 09:01:16 +0100

    Added constructors
    Added constructors for Job & Cluster

--- src/fr/jlc/polytech/scheduler/core/Job.java
+++ src/fr/jlc/polytech/scheduler/core/Job.java
@@ -1,7 +1,21 @@
 package fr.jlc.polytech.scheduler.core;
 
+import com.sun.istack.internal.Nullable;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Job extends ArrayList<Task> implements Serializable {
+	
+	public Job(@Nullable Task... tasks) {
+		super(tasks != null ? tasks.length : 3);
+		
+		if (tasks != null)
+			for (Task task : tasks)
+				if (task != null)
+					add(task);
+	}
+	public Job(int initialCapacity) {
+		super(initialCapacity);
+	}
 }
