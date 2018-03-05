780ee524e9ba05fec41f14ee4d7eb6a496a6517d
Valentin Berger <valentin.berger38@gmail.com>
2018-03-05 10:42:41 +0100

    * Added methods in Box * The FileGenerator is complete! :D

--- src/fr/jlc/polytech/scheduler/io/FileGenerator.java
+++ src/fr/jlc/polytech/scheduler/io/FileGenerator.java
@@ -7,6 +7,8 @@ import fr.jlc.polytech.scheduler.core.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.IllegalFormatException;
+import java.util.Objects;
 
 public class FileGenerator {
 	
@@ -57,7 +59,9 @@ public class FileGenerator {
 		if (box == null)
 			throw new NullPointerException();
 		
-		if (box.getClusters().isEmpty() || box.getJobs().isEmpty())
+		if (!((box.getClusters().isEmpty() && !box.getJobs().isEmpty()) ||
+			 (!box.getClusters().isEmpty() && box.getJobs().isEmpty()) ||
+			 (!box.getClusters().isEmpty() && !box.getJobs().isEmpty())))
 			throw new IllegalArgumentException("There is no cluster nor jobs in the box.");
 		
 		StringBuilder build = new StringBuilder();
@@ -150,9 +154,261 @@ public class FileGenerator {
 	}
 	
 	
-	public static @Nullable Box readBox() {
-		System.out.println(readContent());
-		return null;
+	public static @NotNull Box readBox() {
+		String content = readContent();
+		Box box = new Box();
+		
+		final String err_format = "The file format is not valid.";
+		final String err_job = err_format + " The job number \"%s\" is not valid";
+		final String err_found = err_format + " Found \"%s\" instead.";
+		
+		if (content.isEmpty())
+			return box;
+		
+		/**
+		 * Contains the lines of the content
+		 */
+		String[] lines = content.split("\n");
+		
+		/**
+		 * mode indicates if the method parses servers or job
+		 * If mode == 0: It parses servers
+		 * If mode > 0: It parses the job n°"mode"
+		 */
+		int mode = 0;
+		// TODO: Skip #-lines
+		
+		// The current job to process (null if mode == 0)
+		Job job = null;
+		
+		// This ArrayList contains all the tasks for a job. for each new job, the scope is reset. It is used for the dependencies
+		HashMap<String, Task> scope = new HashMap<>();
+		int nextTaskNumber = 1;
+		
+		// For each line in lines
+		for (String line : lines) {
+			
+			// If the line is "Servers" or "Job #", change the mode
+			if (line.toLowerCase().equals("servers") || line.toLowerCase().startsWith("job"))
+			{
+				// If the line is "Servers", change the mode to 0
+				if (line.toLowerCase().equals("servers"))
+					mode = 0;
+				// Otherwise, change the mode to the number of the job
+				else {
+					// Replace all possible alphabetic-characters and whitespaces by nothing to have the digit at the end
+					line = line.replaceAll(" ", "");
+					line = line.replaceAll("\t", "");
+					line = line.replaceAll(":", "");
+					
+					line = line.substring(3, line.indexOf("="));
+					
+					// Get the number of the job
+					int jobNumber = -1;
+					
+					try {
+						jobNumber = Integer.valueOf(line);
+					} catch (NumberFormatException ex) {
+						throw new UnsupportedOperationException(String.format(err_job, line));
+					}
+					
+					if (jobNumber <= 0)
+						throw new UnsupportedOperationException(String.format(err_job, jobNumber));
+					
+					// Change the mode
+					mode = jobNumber;
+					
+					// Save the last job
+					if (job != null)
+						box.addJob(job);
+					
+					// Instantiate the current job
+					job = new Job();
+				}
+				
+				scope.clear();
+				nextTaskNumber = 1;
+				continue;
+			}
+			
+			// If the line is not "Servers" or "Job #", it is either a list of machines, or a task, depending on mode value
+			
+			// If mode is SERVERS
+			if (mode == 0) {
+				// The line is a list of machine, either CPU, GPU or I/O machines
+				
+				line = line.replaceAll(" ", "");
+				line = line.replaceAll("=", "");
+				line = line.replaceAll("\t", "");
+				
+				Type type;
+				
+				if (line.startsWith("CPU"))
+					type = Type.CPU;
+				else if (line.startsWith("GPU"))
+					type = Type.GPU;
+				else if (line.startsWith("I/O"))
+					type = Type.IO;
+				else
+					throw new UnsupportedOperationException(err_format);
+				
+				// Get all the machines
+				line = line.substring(3);
+				line = line.replaceAll("\\[", "");
+				line = line.replaceAll("]", "");
+				String[] s_capacity = line.split(",");
+				
+				if (s_capacity == null)
+					throw new UnsupportedOperationException(err_format);
+				
+				if (s_capacity.length == 0)
+					throw new UnsupportedOperationException(err_format);
+				
+				ArrayList<Machine> machines = new ArrayList<>(s_capacity.length);
+				
+				for (String sc : s_capacity) {
+					Capacity capacity = fromString(sc);
+					
+					machines.add(new Machine(type, capacity));
+				}
+				
+				box.addMachines(machines);
+			}
+			// If mode is JOB
+			else {
+				Type type = null;
+				Capacity capacity = null;
+				ArrayList<Task> dependencies = new ArrayList<>();
+				
+				line = line.replaceAll("\t", "");
+				
+				// Get the tak number
+				if (!line.toLowerCase().startsWith("t"))
+					throw new UnsupportedOperationException(String.format(err_found, line));
+				
+				// Delete the 'T'
+				line = line.substring(1);
+				
+				// Delete whitespaces
+				line = line.replaceAll(" ", "");
+				
+				// Copy the line (it will be useful after fetching the task number)
+				String c_line = "" + line;
+				
+				c_line = c_line.substring(0, c_line.indexOf("="));
+				
+				int taskNumber = -1;
+				
+				try {
+					taskNumber = Integer.valueOf(c_line);
+				} catch (NumberFormatException ex) {
+					throw new UnsupportedOperationException(err_format);
+				}
+				
+				// Now, get the type, capacity and dependencies
+				c_line = line.substring(line.indexOf("=")+1);
+				String[] parameters = c_line.split(",", 3); // TODO: Might not work
+				
+				if (parameters.length != 3)
+					throw new UnsupportedOperationException(err_format);
+				
+				// Get the type
+				for (Type t : Type.values()) {
+					if (t.toString().equals(parameters[0])) {
+						type = t;
+						break;
+					}
+				}
+				
+				// If type not found
+				if (type == null)
+					throw new UnsupportedOperationException(err_format);
+				
+				// Get the capacity
+				capacity = fromString(parameters[1]);
+				
+				// Get the dependencies
+				parameters[2] = parameters[2].replaceAll("\\[", "");
+				parameters[2] = parameters[2].replaceAll("]", "");
+				
+				if (!parameters[2].isEmpty()) {
+					String[] s_dep = parameters[2].split(",");
+					
+					for (String dep : s_dep) {
+						if (scope.get(dep) == null)
+							throw new UnsupportedOperationException(String.format(err_found, dep));
+						
+						dependencies.add(scope.get(dep));
+					}
+				}
+				
+				Task task = new Task(type, capacity, dependencies);
+				job.add(task);
+				
+				scope.put("T" + taskNumber, task);
+			}
+		}
+		
+		// Save the last job
+		if (job != null)
+			box.addJob(job);
+		
+		return box;
+	}
+	
+	// TODO: place the 3 following methods in Capacity
+	
+	/**
+	 * Parse a string to convert into a capacity
+	 * @param value Value must be a capacity like "40G", "9", "50T", "400 ", ...
+	 * @return Return the instance of the capacity
+	 * @throws UnsupportedOperationException Thrown when the string is not valid
+	 */
+	public static Capacity fromString(String value) {
+		UnsupportedOperationException exception = new UnsupportedOperationException("Unsupported format");
+		
+		boolean parseValue = true;
+		StringBuilder s_value = new StringBuilder();
+		char scale = ' ';
+		
+		for (int i = 0; i < value.length(); i++) {
+			char c = value.charAt(i);
+			
+			if (Character.isDigit(c) && parseValue)
+				s_value.append(c);
+			// If c is ' ' or a scale character...
+			else if (isScaleValid(c) && parseValue) {
+				parseValue = false;
+				scale = c;
+			}
+			else
+				throw exception;
+		}
+		
+		if (s_value.toString().length() == 0)
+			throw exception;
+		
+		long l_value;
+		
+		try {
+			l_value = Long.valueOf(s_value.toString());
+		} catch (NumberFormatException ex) {
+			throw exception;
+		}
+		
+		return new Capacity(l_value, scale);
+	}
+	
+	public static char[] getAllScales() {
+		return new char[] { ' ', 'K', 'M', 'G', 'T' };
+	}
+	
+	public static boolean isScaleValid(char c) {
+		for (char scale : getAllScales())
+			if (scale == c)
+				return true;
+		
+		return false;
 	}
 	
 	public static @NotNull String readContent() {
